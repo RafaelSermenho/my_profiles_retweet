4d0f1f79d34093473a0aeef004b4983e26181cd1
Rafael <rafael.sermenho@gmail.com>
2017-05-04 00:11:13 -0300

    Preparing for migration to linode

--- myProfileBot.js
+++ myProfileBot.js
@@ -10,20 +10,16 @@ var app = express();
 var Twitter = new twit(config);
 
 app.use(bodyParser.json());
-app.get('/', function (req, res) {
-    res.json({ version: packageInfo.version });
-});
+app.get('/myProfile_bot', function (req, res) {
+    res.json({name: "My Profile Bot", version: packageInfo.version});
+})
 
-var server = app.listen(process.env.PORT, "0.0.0.0", function () {
+var server = app.listen(8082, "127.0.0.1", function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Web server started at http://%s:%s', host, port);
 });
 
-
-var users = {
-	nivelBarragemDf: '850538373001486338',
-	galaktikos_bot: '851628825213587456'};
 
 var tweetStream = Twitter.stream('statuses/filter', { follow: '850538373001486338, 851628825213587456'});
 
@@ -36,8 +32,3 @@ tweetStream.on('tweet', function (tweet) {
         }
     });
 });
-
-
-setInterval(function () {
-    https.get("https://nivelbarragemdf-bot.herokuapp.com");
-}, 300000);
