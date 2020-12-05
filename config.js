332ee7435e05f181de36ce7a4de5e7e196220086
rafael.sermenho <rafael.sermenho@dextra-sw.com>
2020-12-05 16:32:09 -0300

    updating version

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {  
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '-',
-  access_token_secret: ''
+  consumer_key: 'Bz54jRCqHw5RTMoyuGW6kJt02',
+  consumer_secret: 'fsl7RGevplB3o0TYd2UmIu1razHF3prubUl4Y2gA2g9O5YXcUv',
+  access_token: '51598325-a6qDcCrGjz7E0HNWvKq2mhKstf8BmCOUHJ9Y6W9XG',
+  access_token_secret: 'MkclJHabHW3ErFRYNVDeLBeFIA4s7cTphV2MARpG3fGXI'
 }
